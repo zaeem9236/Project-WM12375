01664f40acb292d0ea9ce91a47ec4ea6bee4e0b7
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-13 02:22:18 +0500

    nacbar completed

--- olx_clone/src/components/Header/Header.js
+++ olx_clone/src/components/Header/Header.js
@@ -3,31 +3,56 @@ import './Header.css';
 import olx_logo from './olx_logo.png';
 import capture from './Capture.JPG';
 import search_icon from './search_icon.png';
+import SearchIcon from '@material-ui/icons/Search';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 
 
 function Header() {
     return (
         <React.Fragment>
-            <img src={capture}></img>
+            {/* <img src={capture}></img> */}
 
             <div className='header'>
                 <img className='olx_logo' src={olx_logo}></img>
 
                 <div className='searchBar1'>
-                    {/* <img className='searchBar1_icon' src={search_icon}></img> */}
-                    <img className='bi bi-search'></img>
+                    <SearchIcon className='searchBar1_icon'/>
                     <input className='searchBar1_Input' type='text'></input>
                 </div>
 
                 <div className='searchBar2'>
                     <input className='searchBar2_Input' type='text'></input>
                     <div className='searchButtonBox'>
-                    <img className='searchBar2_icon' src={search_icon}></img>
+                    <SearchIcon className='searchBar2_icon'/>
                     </div>
                 </div>
-                <p>login button</p>
-                <p>sell button</p>
+                
+                <div className='login_Div'>
+                <p className='login_Button'>Login</p>
+                </div>
+
+                <div className='sell_Div'>
+                <p className='sell_Button'>+ sell</p>
+                </div>
             </div>
+
+            <div className='bottom_header'>
+                <div className='all_categories'>
+                    <p className='all_categories_text' > ALL CATEGORIES</p>
+                    <KeyboardArrowDownIcon className='down_arrow'/>
+                </div>
+
+                <div className='popular_categories'>
+                    <p className='popular_categories_text'>Mobile Phones</p>
+                    <p className='popular_categories_text'>Cars</p>
+                    <p className='popular_categories_text'>Motorcycles</p>
+                    <p className='popular_categories_text'>Houses</p>
+                    <p className='popular_categories_text'>TV-Video-Audio</p>
+                    <p className='popular_categories_text'>Tablets</p>
+                    <p className='popular_categories_text'>Land & Plots</p>
+                </div>
+            </div>
+        
         </React.Fragment>
     );
 }
