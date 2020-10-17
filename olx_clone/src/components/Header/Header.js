9c7353cad847821a04ca8d4c5726dc70a85f7313
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-17 21:17:56 +0500

    header.js updated

--- olx_clone/src/components/Header/Header.js
+++ olx_clone/src/components/Header/Header.js
@@ -62,27 +62,159 @@ function Header() {
             <div className='drop_down_categories'>
                     <div className={`drop_down_list ${drop_down_visible ? '' : 'drop_down_list_hide'}`}>
                         <ul className='col_1'>
-                            <li>house</li>
-                            <li>flat</li>
-                            <li>plot</li>
+                            <li className='ul_heading' >Vehicles</li>
+                            <li>Tractors & Trailers</li>
+                            <li>Boats</li>
+                            <li>Other Vehicles</li>
+                            <li>Rickshaw & Chingchi</li>
+                            <li>Buses, Vans & Trucks</li>
+                            <li>Spare Parts</li>
+                            <li>Cars Accessories</li>
+                            <li>Cars on Installments</li>
+                            <li>Cars</li>
+
+                            <li className='ul_heading' >Mobiles</li>
+                            <li>Mobile Phones</li>
+                            <li>Accessories</li>
+                            <li>Tablets</li>
+
+                            <li className='ul_heading' >Electronics & Home Appliances</li>
+                            <li>Washing Machines & Dryers</li>
+                            <li>Fridges & Freezers</li>
+                            <li>AC & Coolers</li>
+                            <li>Kitchen Appliances</li>
+                            <li>Generators, UPS & Power Solutions</li>
+                            <li>Other Home Appliances</li>
+                            <li>Games & Entertainment</li>
+                            <li>Cameras & Accessories</li>
+                            <li>TV - Video - Audio</li>
+                            <li>Computers & Accessories</li>
+
+                            <li className='ul_heading' >Property for Sale</li>
+                            <li>Portions & Floors</li>
+                            <li>Shops - Offices - Commercial Space</li>
+                            <li>Apartments & Flats</li>
+                            <li>Houses</li>
+                            <li>Land & Plots</li>
                         </ul>
 
                         <ul className='col_2'>
-                            <li>hatchback</li>
-                            <li>suv</li>
-                            <li>jeep</li>
+                            <li  className='ul_heading' >Animals</li>
+                            <li>Other Animals</li>
+                            <li>Pet Food & Accessories</li>
+                            <li>Horses</li>
+                            <li>Livestock</li>
+                            <li>Dogs</li>
+                            <li>Cats</li>
+                            <li>Hens & Aseel</li>
+                            <li>Birds</li>
+                            <li>Fish & Aquariums</li>
+
+                            <li className='ul_heading' >Furniture & Home Decor</li>
+                            <li>Other Household Items</li>
+                            <li>Office Furniture</li>
+                            <li>Curtains & Blinds</li>
+                            <li>Rugs & Carpets</li>
+                            <li>Painting & Mirrors</li>
+                            <li>Garden & Outdoor</li>
+                            <li>Tables & Dining</li>
+                            <li>Home Decoration</li>
+                            <li>Beds & Wardrobes</li>
+                            <li>Sofa & Chairs</li>
+
+                            <li className='ul_heading' >Business, Industrial & Agriculture</li>
+                            <li>Medical & Pharma</li>
+                            <li>Other Business & Industry</li>
+                            <li>Agriculture</li>
+                            <li>Construction & Heavy Machinery</li>
+                            <li>Trade & Industrial</li>
+                            <li>Food & Restaurants</li>
+                            <li>Business for Sale</li>
+
+                            <li className='ul_heading' >Bikes</li>
+                            <li>Scooters</li>
+                            <li>ATV & Quads</li>
+                            <li>Bicycles</li>
+                            <li>Spare Parts</li>
+                            <li>Motorcycles</li>
+
                         </ul>
 
                         <ul className='col_3'>
-                            <li>telephone</li>
-                            <li>mobile</li>
-                            <li>tablets</li>
+                            <li  className='ul_heading' >Fashion & Beauty</li>
+                            <li>Other Fashion</li>
+                            <li>Couture</li>
+                            <li>Lawn & Pret</li>
+                            <li>Wedding</li>
+                            <li>Watches</li>
+                            <li>Skin & Hair</li>
+                            <li>Make Up</li>
+                            <li>Jewellery</li>
+                            <li>Footwear</li>
+                            <li>Clothes</li>
+                            <li>Accessories</li>
+
+                            <li className='ul_heading' >Property for Rent</li>
+                            <li>Land & Plots</li>
+                            <li>Vacation Rentals - Guest Houses</li>
+                            <li>Roommates & Paying Guests</li>
+                            <li>Rooms</li>
+                            <li>Shops - Offices - Commercial Space</li>
+                            <li>Portions & Floors</li>
+                            <li>Apartments & Flats</li>
+                            <li>Houses</li>
+
+                            <li className='ul_heading' >Jobs</li>
+                            <li>Other Jobs</li>
+                            <li>Part - Time</li>
+                            <li>Domestic Staff</li>
+                            <li>Medical</li>
+                            <li>Manufacturing</li>
+                            <li>Accounting & Finance</li>
+                            <li>Human Resources</li>
+                            <li>Clerical & Administration</li>
+                            <li>Hotels & Tourism</li>
+                            <li>IT & Networking</li>
+                            <li>Sales</li>
+                            <li>Customer Service</li>
+                            <li>Education</li>
+                            <li>Advertising & PR</li>
+                            <li>Marketing</li>
+                            <li>Online</li>
+                            
                         </ul>
 
                         <ul className='col_4'>
-                            <li>pajama</li>
-                            <li>clothes</li>
-                            <li>pants</li>
+                            <li className='ul_heading' >Services</li>
+                            <li>Farm & Fresh Food</li>
+                            <li>Catering & Restaurant</li>
+                            <li>Home & Office Repair</li>
+                            <li>Movers & Packers</li>
+                            <li>Maids & Domestic Help</li>
+                            <li>Health & Beauty</li>
+                            <li>Event Services</li>
+                            <li>Electronics & Computer Repair</li>
+                            <li>Other Services</li>
+                            <li>Web Development</li>
+                            <li>Drivers & Taxi</li>
+                            <li>Car Rental</li>
+                            <li>Travel & Visa</li>
+                            <li>Education & Classes</li>
+
+                            <li className='ul_heading' >Books, Sports & Hobbies</li>
+                            <li>Other Hobbies</li>
+                            <li>Gym & Fitness</li>
+                            <li>Sports Equipment</li>
+                            <li>Musical Instruments</li>
+                            <li>Books & Magazines</li>
+
+                            <li className='ul_heading' >Kids</li>
+                            <li>Kids Accessories</li>
+                            <li>Kids Bikes</li>
+                            <li>Swings & Slides</li>
+                            <li>Prams & Walkers</li>
+                            <li>Toys</li>
+                            <li>Kids Furniture</li>
                         </ul>
                     </div>
 
