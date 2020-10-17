e5a3cbd7eae4ae17caf82af51fe1d107087f8aac
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-17 22:32:24 +0500

    Footer.js created

--- olx_clone/src/App.js
+++ olx_clone/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Header } from './components/Header/Header.js'
+import { Header } from './components/Header/Header.js';
+import { Footer } from './components/Footer/Footer.js';
 import './App.css';
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
           <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
           <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
         </div>
+
+        <Footer></Footer>
       
       </div>
     </React.Fragment>
---
+++ olx_clone/src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return(
+        <h1 className='cheading' >this is footer section</h1>
+    );
+}
+
+export { Footer};
