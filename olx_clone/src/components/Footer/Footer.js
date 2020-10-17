26c022d57ff30652b47f2dbede798510eba8264e
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-17 23:06:31 +0500

    footer.js updated

--- olx_clone/src/components/Footer/Footer.js
+++ olx_clone/src/components/Footer/Footer.js
@@ -1,10 +1,50 @@
 import React from 'react';
 import './Footer.css'
+import topFooter from './olx_top_footer.jpg';
 
 function Footer() {
-    return(
-        <h1 className='cheading' >this is footer section</h1>
+    return (
+        <React.Fragment>
+            <div className='top_footer'>
+                <img src={topFooter} ></img>
+            </div>
+
+            <div className='middle_footer' >
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                
+                <ul>
+                    <p>col3</p>
+                </ul>
+
+                <ul>
+                    <p>col4</p>
+
+                </ul>
+
+                <ul>
+                    <p>col5</p>
+                </ul>
+            </div>
+
+            <div className='bottom_footer'>
+                <p>botton footer</p>
+            </div>
+        </React.Fragment>
     );
 }
 
-export { Footer};
+export { Footer };
