d15f1f068a917cfc0d5cbb6d11217b8acd56a241
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-29 00:22:14 +0500

    cards updated

--- olx_clone/src/App.js
+++ olx_clone/src/App.js
@@ -15,28 +15,44 @@ function App() {
         {console.log(featured_products[0].picture)};
         {/* {console.log(products)}; */}
         {/* <img src={Card_pic} alt=''></img> */}
-        
+
         <div className='featured_items_display'>
           <div className='featured_items_title'><p>Featured Items</p></div>
           <div className='featured_products'>
-            <Cards picture={featured_products[0].picture} 
-            price={featured_products[0].price} 
-            description={featured_products[0].description} 
-            />
-            </div>
+            {featured_products.map(function (featured_products_Obj, index) {
+              return (
+                <Cards picture={featured_products[index].picture}
+                  price={`Rs  ${featured_products[index].price}`}
+                  description={featured_products[index].description}
+                />
+              );
+            })}
+          </div>
+        </div>
+
+        <div className='fresh_recommendations'>
+          <div className='fresh_title'><p>Featured Items</p></div>
+          <div className='fresh_products'>
+            {products.map(function (products_Obj, index) {
+              return (
+                <Cards picture={products[index].picture}
+                  price={`Rs  ${products[index].price}`}
+                  description={products[index].description}
+                />
+              );
+            })}
+            
+          </div>
         </div>
         
-
-        <div>
-          <h1>pakhair raghalay</h1>
-          <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
-          <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
-          <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
-          <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
-        </div>
+        <h1>pakhair raghalay</h1>
+        <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
+        <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
+        <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
+        <h1>s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</h1>
 
         <Footer></Footer>
-      
+
       </div>
     </React.Fragment>
   );
--- olx_clone/src/components/Products/Products.js
+++ olx_clone/src/components/Products/Products.js
@@ -1,11 +1,59 @@
 
 
-    export let featured_products = [
-        {
-            picture: 'https://picsum.photos/seed/picsum/200/300',
-            price: '1234',
-            description: 'lorem pic'
-        }
-    ];
-    
-    export let products = [{name:'zaeem',value:'billions'},'2','3','5','6'];
+export let featured_products = [
+    {
+        picture: 'https://picsum.photos/200/300',
+        price: '1',
+        description: 'lorem pic'
+    },
+
+    {
+        picture: 'https://picsum.photos/200/300',
+        price: '2',
+        description: 'lorem pic'
+    },
+
+    {
+        picture: 'https://picsum.photos/200/300',
+        price: '3',
+        description: 'lorem pic'
+    },
+
+    {
+        picture: 'https://picsum.photos/200/300',
+        price: '4',
+        description: 'lorem pic'
+    }
+];
+
+export let products = [
+    {
+        picture: 'https://picsum.photos/seed/picsum/200/300',
+        price: '1',
+        description: 'lorem pic'
+    },
+
+    {
+        picture: 'https://picsum.photos/seed/picsum/200/300',
+        price: '1',
+        description: 'lorem pic'
+    },
+
+    {
+        picture: 'https://picsum.photos/seed/picsum/200/300',
+        price: '1',
+        description: 'lorem pic'
+    },
+
+    {
+        picture: 'https://picsum.photos/seed/picsum/200/300',
+        price: '1',
+        description: 'lorem pic'
+    },
+
+    {
+        picture: 'https://picsum.photos/seed/picsum/200/300',
+        price: '1',
+        description: 'lorem pic'
+    }
+];
